e11edb05411679c7337f4b6cd46aa2990299d4b7
dmitro <dima25122018@gmail.com>
2026-03-31 01:16:45 +0300

    update product info

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -12,11 +12,18 @@ export type Category = {
 export type Product = {
   id: number;
   name: string;
+  producer: string;
+  type: string;
   description: string;
+  title: string | null;
   cost: number;
+  weight: number | null;
+  unit: string | null;
+  guarantee: string | null;
   image_url: string;
   category_id: number;
   created_at: string;
+  updated_at: string;
 };
 
 export type CartItem = {
